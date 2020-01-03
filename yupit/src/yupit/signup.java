96197a7eccbf3ff1a40b0ac776f98340a32af5ed
QL <QL@admin-PC>
2020-01-03 19:20:48 +0530

    tewst

--- yupit/src/yupit/signup.java
+++ yupit/src/yupit/signup.java
@@ -5,7 +5,7 @@ public class signup {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
               System.out.println("signup again");
-              System.out.println("try  another one   commit");
+              System.out.println("try  another one  commit");
 
               
 	}
